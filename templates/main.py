86efa0c55b96233f04b28e2eff7bd0a073386828
Franco Cusmai <fcusmai@b2gov.com>
2021-12-02 11:50:09 -0300

    game template v1

---
+++ templates/main.py
@@ -0,0 +1,122 @@
+import pygame, sys, time
+
+from pygame.locals import *
+from pygame import Vector2
+
+from constants import *
+
+
+class Game():
+    def __init__(self, game_name: str) -> None:
+        # Pygame Init ------------------------------------- #
+        pygame.init()
+
+        # Display Settings -------------------------------- #
+        self.scale = 2
+        self.display_size = Vector2(DISPLAY_SIZE)
+        self.screen_size = Vector2(SCREEN_SIZE) * self.scale
+        self.display = pygame.Surface(self.display_size)
+        self.screen = pygame.display.set_mode((self.screen_size), RESIZABLE | DOUBLEBUF, DEFAULT_BPP)
+        self.game_name = game_name
+        pygame.display.set_caption(game_name)
+
+        # Font -------------------------------------------- #
+        self.font = pygame.font.SysFont('verdana', 10)
+
+        # Clock ------------------------------------------- #
+        self.main_clock = pygame.time.Clock()
+        self.last_time = time.time()
+        self.fps = self.main_clock.get_fps()
+        self.fps_display = ''
+        self.fps_show = True
+
+        # Keys pressed ------------------------------------ #
+        self.keys = {
+            K_LCTRL: 0,
+            K_p: 0,
+        }
+
+
+    # Run Game ---------------------------------- #
+    def run(self) -> None:
+        while 1:
+            dt = self.delta_time()
+            self.process_events()
+            self.update(dt)
+            self.draw()
+
+
+    # Handle events ----------------------------- #
+    def process_events(self) -> None:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                self.quit()
+
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    self.quit()
+
+                if event.key == K_LCTRL:
+                    self.keys[K_LCTRL] = 1
+
+                if event.key == K_p:
+                    self.keys[K_p] = 1
+
+            if event.type == KEYUP:
+                if event.key == K_LCTRL:
+                    self.keys[K_LCTRL] = 0
+
+                if event.key == K_p:
+                    self.keys[K_p] = 0
+
+
+    # Update Game ------------------------------- #
+    def update(self, dt) -> None:
+        self.fps_update()
+
+        if self.keys[K_LCTRL] and self.keys[K_p]:
+            self.fps_show = not self.fps_show
+
+
+    # Draw Game --------------------------------- #
+    def draw(self):
+        self.display.fill((30,20,30))
+
+        # Layers ------------------------------------- #
+        layer = pygame.Surface(self.display.get_size(), SRCALPHA)
+        self.fps_render(layer)
+        self.display.blit(layer, Vector2())
+
+        pygame.transform.scale(self.display, self.screen.get_size(), self.screen)
+        pygame.display.flip()
+
+
+    # Exit Game --------------------------------- #
+    def quit(self) -> None:
+        pygame.quit()
+        sys.exit()
+
+
+    # Delta Time -------------------------------- #
+    def delta_time(self) -> float:
+        self.main_clock.tick(0)
+        self.fps = self.main_clock.get_fps()
+        dt = time.time() - self.last_time
+        self.last_time = time.time()
+        return dt
+
+
+    # Fps Update -------------------------------- #
+    def fps_update(self) -> None:
+        fps_str = str(round(self.fps, 2))
+        text = self.font.render(fps_str, True, (255, 255, 255))
+        self.fps_display = text
+
+
+    # Fps Render -------------------------------- #
+    def fps_render(self, layer: pygame.surface.Surface) -> None:
+        if self.fps_show:
+            layer.blit(self.fps_display, (self.display_size[0] - 40, 5))
+
+
+Game(GAME_NAME).run()
