5346d983716005f65bb14cdc95b9af17754d0c0d
Franco Cusmai <fcusmai@b2gov.com>
2021-12-27 13:19:03 -0300

    top down weapons

---
+++ scripts/top_down_weapons/main.py
@@ -0,0 +1,164 @@
+import pygame, sys, time
+
+from pygame.locals import *
+from pygame import Vector2
+
+from constants import *
+from top_down_weapons import Bullet, Weapon
+
+
+class Game():
+    def __init__(self, game_name: str) -> None:
+        # Pygame Init ------------------------------------- #
+        pygame.init()
+
+        # Display Settings -------------------------------- #
+        self.scale = GAME_SCALE
+        self.display_size = Vector2(DISPLAY_SIZE)
+        self.screen_size = Vector2(DISPLAY_SIZE) * self.scale
+        self.display = pygame.Surface(self.display_size)
+        self.screen = pygame.display.set_mode((self.screen_size), DOUBLEBUF, DEFAULT_BPP)
+        self.game_name = game_name
+        pygame.display.set_caption(game_name)
+
+        # Font -------------------------------------------- #
+        self.font = pygame.font.SysFont('verdana', 10)
+
+        # Clock ------------------------------------------- #
+        self.main_clock = pygame.time.Clock()
+        self.last_time = time.time()
+        self.fps = self.main_clock.get_fps()
+        self.fps_display = ''
+        self.fps_show = True
+        self.fps_changed = False
+
+        # Keys pressed ------------------------------------ #
+        self.keys = {
+            K_LCTRL: 0,
+            K_p: 0,
+            KEYUP: 0,
+            KEYDOWN: 0
+        }
+
+        self.weapon_image = pygame.surface.Surface((40, 10))
+        self.weapon_image.set_colorkey((0,0,0))
+        self.weapon_image.fill((120,120,120))
+        self.weapon = Weapon(self.weapon_image, pos_canon=Vector2(18, 0))
+
+        self.bullet_image = pygame.Surface((4,4))
+        self.bullet_image.set_colorkey(COLOR_KEY_0)
+        pygame.draw.circle(self.bullet_image, BLUE, Vector2(4,4) / 2, 2)
+        pygame.draw.circle(self.bullet_image, WHITE, Vector2(4,4) / 2, 1)
+        self.bullets = [Bullet(self.bullet_image) for x in range(50)]
+
+
+    # Run Game ---------------------------------- #
+    def run(self) -> None:
+        while 1:
+            dt = self.delta_time()
+            self.process_events()
+            self.update(dt)
+            self.draw()
+
+
+    # Handle events ----------------------------- #
+    def process_events(self) -> None:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                self.quit()
+
+            if event.type == KEYDOWN:
+                self.keys[KEYDOWN] = 1
+                self.keys[KEYUP] = 0
+                
+                if event.key == K_ESCAPE:
+                    self.quit()
+
+                if event.key == K_LCTRL:
+                    self.keys[K_LCTRL] = 1
+
+                if event.key == K_p:
+                    self.keys[K_p] = 1
+
+            if event.type == KEYUP:
+                self.keys[KEYDOWN] = 0
+                self.keys[KEYUP] = 1
+
+                if event.key == K_LCTRL:
+                    self.keys[K_LCTRL] = 0
+
+                if event.key == K_p:
+                    self.keys[K_p] = 0
+
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == BUTTON_LEFT:
+                    self.weapon.shoot(self.bullets)
+
+
+    # Update Game ------------------------------- #
+    def update(self, dt) -> None:
+        pointer = Vector2(pygame.mouse.get_pos())
+
+        self.fps_update()
+
+        if self.keys[K_LCTRL] and self.keys[K_p] and not self.fps_changed:
+            self.fps_show = not self.fps_show
+            self.fps_changed = True
+
+        if self.keys[KEYUP]:
+            self.fps_changed = False
+
+        self.weapon.update(dt, self.display_size / 2, pointer)
+
+        for bullet in self.bullets:
+            bullet.update(dt)
+
+
+    # Draw Game --------------------------------- #
+    def draw(self):
+        self.display.fill(BK_COLOR)
+
+        # Layers ------------------------------------- #
+        layer0 = pygame.Surface(self.display.get_size(), SRCALPHA)
+        self.fps_render(layer0)
+        self.weapon.render(layer0)
+
+        for bullet in self.bullets:
+            bullet.render(layer0)
+
+        # Blit Layers -------------------------------- #
+        self.display.blit(layer0, Vector2())
+
+        pygame.transform.scale(self.display, self.screen.get_size(), self.screen)
+        pygame.display.flip()
+
+
+    # Exit Game --------------------------------- #
+    def quit(self) -> None:
+        pygame.quit()
+        sys.exit()
+
+
+    # Delta Time -------------------------------- #
+    def delta_time(self) -> float:
+        self.main_clock.tick(FPS_LIMIT)
+        self.fps = self.main_clock.get_fps()
+        dt = time.time() - self.last_time
+        self.last_time = time.time()
+        return dt
+
+
+    # Fps Update -------------------------------- #
+    def fps_update(self) -> None:
+        fps_str = 'FPS: ' + str(round(self.fps, 2))
+        text = self.font.render(fps_str, True, WHITE)
+        self.fps_display = text
+
+
+    # Fps Render -------------------------------- #
+    def fps_render(self, layer: pygame.surface.Surface) -> None:
+        if self.fps_show:
+            layer.blit(self.fps_display, (self.display_size[0] - 65, 5))
+
+
+Game(GAME_NAME).run()
---
+++ scripts/top_down_weapons/top_down_weapons.py
@@ -0,0 +1,161 @@
+from random import randint
+from math import cos, sin, pi
+from typing import Counter
+
+import pygame
+from pygame import Vector2
+
+from constants import *
+
+
+vec = Vector2
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, image: pygame.Surface, **kwargs) -> None:
+        pygame.sprite.Sprite.__init__(self)
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.disabled = kwargs.get('disabled') or 1
+
+        self.pos = kwargs.get('pos') or vec(0,0)
+        self.vel = kwargs.get('vel') or vec(0,0)
+
+        self.dmg = kwargs.get('dmg') or 0
+
+
+    def update(self, dt) -> None:
+        if self.disabled:
+            return
+
+        self.pos += self.vel * dt
+        self.rect.update((self.pos), (self.image.get_size()))
+
+        if self.pos[0] > DISPLAY_SIZE[0]:
+            self.disabled = 1
+        if self.pos[0] < 0:
+            self.disabled = 1
+        if self.pos[1] > DISPLAY_SIZE[1]:
+            self.disabled = 1
+        if self.pos[1] < 0:
+            self.disabled = 1
+
+
+    def render(self, surface: pygame.Surface) -> None:
+        if self.disabled:
+            return
+
+        surface.blit(self.image, self.pos)
+
+
+
+class Weapon(pygame.sprite.Sprite):
+    def __init__(self, image: pygame.Surface, **kwargs) -> None:
+        pygame.sprite.Sprite.__init__(self)
+        self.image = image
+        self.angle = 0
+        self.rect = self.image.get_rect()
+
+        self.pos = kwargs.get('pos') or vec(0,0)
+        self.pos_canon = kwargs.get('pos_canon') or vec(0,0)
+        self.current_pos_canon = self.pos_canon
+
+        self.accuracy = kwargs.get('accuracy') or 1
+        self.accuracy_recover_rate = kwargs.get('accuracy_recover_rate') or 0.920
+        self.current_accuracy = self.accuracy
+        self.recoil = kwargs.get('recoil') or 10
+        self.cadency = kwargs.get('cadency') or 50
+        self.last_shoot_time = 0
+
+        self.bullet_speed = kwargs.get('bullet_speed') or 500
+        self.bullet_per_shoot = kwargs.get('bullet_per_shoot') or 1
+        self.bullet_dmg = kwargs.get('dmg') or 10
+
+        self.magazine_size = kwargs.get('magazine_size') or 30
+        self.magazine_ammount = kwargs.get('magazine_amount') or 30
+        self.magazine_is_empty = kwargs.get('magazine_is_empty') or 0
+        self.ammo_per_shoot = kwargs.get('ammo_per_shoot') or 1
+
+        self.reload_time = kwargs.get('reload_time') or 1000
+        self.reload_time_init = 0
+        self.reloading = kwargs.get('reloading') or 0
+
+
+    def update(self, dt: float, new_pos: Vector2, pointer: Vector2) -> None:
+        self.pos = new_pos
+        self.current_pos_canon = self.pos_canon + new_pos
+        magnitude = vec(self.pos - self.current_pos_canon).magnitude()
+        self.rect.update((new_pos), (self.image.get_size()))
+        self.angle = vec(new_pos * GAME_SCALE - pointer).angle_to(vec(1,0))
+        self.current_pos_canon = self.pos - vec(new_pos * GAME_SCALE - pointer).normalize() * magnitude
+
+        print(self.current_accuracy)
+
+        if self.current_accuracy > self.accuracy:
+            if self.current_accuracy > 90:
+                self.current_accuracy = 90
+            else:
+                self.current_accuracy *= self.accuracy_recover_rate
+        else:
+            self.current_accuracy = self.accuracy        
+
+        if self.reloading:
+            self.check_reloading()
+
+
+    def render(self, surface: pygame.Surface) -> None:
+        rotated_image = pygame.transform.rotate(self.image, self.angle)
+        surface.blit(rotated_image, self.pos - (vec(rotated_image.get_size()) / 2))
+        pygame.draw.circle(surface, (0,255,0), self.pos, 2)
+        pygame.draw.circle(surface, (0,255,0), self.current_pos_canon, 2)
+
+
+    def shoot(self, bullets: 'list[Bullet]') -> None:
+        if self.reloading:
+            return
+
+        if self.magazine_is_empty:
+            self.reload()
+
+        if pygame.time.get_ticks() - self.last_shoot_time < self.cadency:
+            return
+
+        bullet_pos = vec(self.current_pos_canon)
+
+        for x in range(self.bullet_per_shoot):
+            accuracy = randint(-int(self.current_accuracy), int(self.current_accuracy))
+            bullet_dir = vec(
+                sin((self.angle - 90 - accuracy) * pi / 180),
+                cos((self.angle - 90 - accuracy) * pi / 180),
+            )
+
+            for bullet in bullets:
+                if bullet.disabled:
+                    bullet.disabled = 0
+                    bullet.pos = vec(bullet_pos)
+                    bullet.vel = bullet_dir * self.bullet_speed
+                    bullet.dmg = self.bullet_dmg
+                    break
+                    
+
+        self.current_accuracy *= self.recoil
+        self.last_shoot_time = pygame.time.get_ticks()
+        self.magazine_ammount -= self.ammo_per_shoot
+
+        if self.magazine_ammount == 0:
+            self.magazine_is_empty = 1
+
+
+    def reload(self) -> None:
+        self.reloading = 1
+        self.reload_time_init = pygame.time.get_ticks()
+
+
+    def check_reloading(self) -> None:
+        if pygame.time.get_ticks() - self.reload_time_init > self.reload_time:
+            self.reloading = 0
+            self.magazine_ammount = self.magazine_size
+            self.magazine_is_empty = 0
+
+
+    def unlock_gun(self) -> None:
+        pass
